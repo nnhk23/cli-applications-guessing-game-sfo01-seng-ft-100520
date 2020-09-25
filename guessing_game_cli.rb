1a6a17abeb3628cff2af29ea5dd0b55d8e13e1ef
Kim Nguyen <nnhk23@gmail.com>
2020-09-25 00:58:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,11 @@
-# Code your solution here!
+def run_guessing_game(number)
+  random_number = ""
+  random_number = rand(6) + 1 
+  if number == random_number
+    return "You guessed the correct number!"
+    elsif number == "exit"
+    return "Goodbye!"
+  else
+    return "Sorry! the computer guessed " + random_number + "."
+  end
+end
