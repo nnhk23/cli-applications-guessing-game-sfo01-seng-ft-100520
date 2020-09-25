45122dba106a5914f938fda627094733ae2b4203
Kim Nguyen <nnhk23@gmail.com>
2020-09-25 01:23:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,15 +1,18 @@
+require 'pry'
 def run_guessing_game
   random_number = ""
   input = ""
   result = ""
   input = gets.chomp
-  random_number = rand(6) + 1
+  random_number = (rand(6) + 1).to_s
+  binding.pry
   if input == random_number
     result = "You guessed the correct number!"
-    elsif input == "exit"
+  elsif input == "exit"
     result = "Goodbye!"
   else
     result = "Sorry! The computer guessed "+ random_number + "."
+    binding.pry
   end
   result
 end
