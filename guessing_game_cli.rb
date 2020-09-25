9cd9b275b1022358281014f596c6a8d552c4b363
Kim Nguyen <nnhk23@gmail.com>
2020-09-25 01:34:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   result = ""
-  input = gets.chomp
   puts "Guess a random number between 1 and 6: "
+  input = gets.chomp
   random_number = rand(6) + 1
   if input.to_i == random_number
     puts "You guessed the correct number!"
