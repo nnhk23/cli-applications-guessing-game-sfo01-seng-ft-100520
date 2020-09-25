f84a8a695690ab4ff3623e18a3c49fdc0c9c0ff4
Kim Nguyen <nnhk23@gmail.com>
2020-09-25 01:14:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   random_number = ""
   input = ""
   input = gets.chomp
-  random_number = rand(6) + 1 
+  random_number = (rand(6) + 1).to_s
   if input == random_number
     return "You guessed the correct number!"
     elsif input == "exit"
